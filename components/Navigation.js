1b683a26d086784583ef617d14f459ba88229cc0
johnalbert.bertuldo <johnalbert.bertuldo@gcash.com>
2023-08-03 20:09:49 +0800

    Initial commit

---
+++ components/Navigation.js
@@ -0,0 +1,33 @@
+import {ImStatsDots} from 'react-icons/im'
+
+const Nav = () => {
+    return(
+    <header className="container max-w-2xl px-6 py-6 mx-auto">
+      <div className="flex items-center justify-between">
+        {/* User information */}
+        <div className='flex items-center gap-2'>
+          {/* img */}
+          <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+            <img 
+            className='object-cover w-full h-full'
+            src="https://thispersondoesnotexist.com/" alt="Profile image"/>
+          </div>
+          
+          {/* name */}
+          <small>Hi, Albert!</small>
+        </div>
+
+        {/* Right side of our navigation */}
+        <nav className="flex items-center gap-4">
+            <div>
+              <ImStatsDots className="text-2xl" />
+            </div>
+            <div>
+              <button className="btn btn-danger">Sign out</button>
+            </div>
+        </nav>
+      </div>
+    </header>
+    )
+}
+export default Nav
