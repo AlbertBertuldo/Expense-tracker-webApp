98e1c26f4ba1773e2f647d90f73c02678029669a
johnalbert.bertuldo <johnalbert.bertuldo@gcash.com>
2023-08-04 09:17:20 +0800

    batman commit

--- app/page.js
+++ app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
             }}
             className="btn btn-primary"
           >
-            + Expenses
+            - Expenses
           </button>
           <button
             onClick={() => {
@@ -86,7 +86,7 @@ export default function Home() {
 
         {/* Chart Section */}
         <section className="py-6">
-          <h3 className="text-2xl">Stats</h3>
+          <h3 className="text-2xl">Expenses Chart</h3>
           <div className="w-1/2 mx-auto">
             <Doughnut
               data={{
